2acb2f7610ec29da363b3118d52a768be68ffc75
Ricardo Anderson Yalico Roncal <u201920140@upc.edu.pe>
2026-02-08 20:51:42 -0500

    Actualizando endpoints

--- src/main/java/com/example/dtos/request/ProductRequest.java
+++ src/main/java/com/example/dtos/request/ProductRequest.java
@@ -11,7 +11,6 @@ public class ProductRequest {
     private String name;
     private String description;
     private String fullDescription;
-    private BigDecimal basePrice;
     private boolean active = true;
     private String label;
     private Long categoryId;
